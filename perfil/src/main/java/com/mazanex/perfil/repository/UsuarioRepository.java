715ffca2c656443c3edb4360d851b1cdd30c9c0e
brstockle <br.stockle@duocuc.cl>
2026-05-05 10:32:44 -0400

    Update UsuarioRepository.java

--- perfil/src/main/java/com/mazanex/perfil/repository/UsuarioRepository.java
+++ perfil/src/main/java/com/mazanex/perfil/repository/UsuarioRepository.java
@@ -3,6 +3,7 @@ package com.mazanex.perfil.repository;
 import com.mazanex.perfil.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
