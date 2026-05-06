a23a2f37144ffa8fb294532528fac4dafa2f267a
brstockle <br.stockle@duocuc.cl>
2026-05-06 19:17:04 -0400

    Update SwaggerConfig.java

--- auth/src/main/java/com/mazanex/auth/config/SwaggerConfig.java
+++ auth/src/main/java/com/mazanex/auth/config/SwaggerConfig.java
@@ -2,18 +2,20 @@ package com.mazanex.auth.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SwaggerConfig {
+public class OpenApiConfig {
 
     @Bean
-    public OpenAPI authOpenAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                    .title("API Auth - Mazanex")
-                    .description("Documentación elegante y clara para el microservicio de autenticación")
-                    .version("v1.0"));
+                        .title("Mazanex Auth API")
+                        .version("1.0")
+                        .description("Microservicio encargado de la autenticación y registro de usuarios.")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
