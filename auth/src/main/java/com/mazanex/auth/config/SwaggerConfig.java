ea02e54b1ec7931436ff5ff14de5bd23d2409ccf
brstockle <br.stockle@duocuc.cl>
2026-05-06 19:16:32 -0400

    Add files via upload

---
+++ auth/src/main/java/com/mazanex/auth/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.mazanex.auth.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI authOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                    .title("API Auth - Mazanex")
+                    .description("Documentación elegante y clara para el microservicio de autenticación")
+                    .version("v1.0"));
+    }
+}
